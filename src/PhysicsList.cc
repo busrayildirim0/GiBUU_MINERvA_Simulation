f81b56faf98fae37c70cf7c6dc107fb31912c7f1
BÜŞRA YILDIRIM <143350207+busrayildirim0@users.noreply.github.com>
2025-08-11 23:27:58 +0300

    Update PhysicsList.cc

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -8,18 +8,15 @@
 
 PhysicsList::PhysicsList()
 {
-    // FTFP_BERT fizik listesini factory ile al
     G4PhysListFactory factory;
     G4VModularPhysicsList* physList = factory.GetReferencePhysList("FTFP_BERT");
     
-    // Tüm fizik modüllerini kopyala
     for (G4int i = 0; ; ++i) {
         G4VPhysicsConstructor* physics = const_cast<G4VPhysicsConstructor*>(physList->GetPhysics(i));
         if (!physics) break;
         RegisterPhysics(physics);
     }
     
-    // Ek fizik modülleri
     RegisterPhysics(new G4EmStandardPhysics());
     RegisterPhysics(new G4DecayPhysics());
     RegisterPhysics(new G4RadioactiveDecayPhysics());
